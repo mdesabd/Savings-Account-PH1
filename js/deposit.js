a95ff9f736a85061bd312d3fa357e71c7254e018
mdesabd <esa.ce2020@gmail.com>
2022-10-26 00:02:59 +0600

    home

--- js/deposit.js
+++ js/deposit.js
@@ -23,6 +23,7 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
     // step-6: calculate current total balance
+     // step-6: calculate current total balance
     const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
     // set the balance total
     balanceTotalElement.innerText = currentBalanceTotal;
